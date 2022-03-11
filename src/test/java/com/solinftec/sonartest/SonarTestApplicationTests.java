59849faaba85a32ad47e8452732d50eee6862b45
samuelfalico <samuel_1797@hotmail.com>
2022-03-11 18:24:24 -0300

    Add test using JUnit

--- src/test/java/com/solinftec/sonartest/SonarTestApplicationTests.java
+++ src/test/java/com/solinftec/sonartest/SonarTestApplicationTests.java
@@ -1,13 +1,23 @@
 package com.solinftec.sonartest;
 
+import com.solinftec.sonartest.service.SumService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SonarTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	SumService sumService;
+    @BeforeEach
+    public void setUp() throws Exception {
+        sumService = new SumService();
+    }
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(6, sumService.sum(5, 1));
+    }
 
 }
