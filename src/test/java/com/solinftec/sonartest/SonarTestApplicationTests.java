154ca1f0fd060643263e01a00f5c49aaa4dcd315
samuelfalico <samuel_1797@hotmail.com>
2022-03-11 16:45:00 -0300

    first commit

---
+++ src/test/java/com/solinftec/sonartest/SonarTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solinftec.sonartest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
